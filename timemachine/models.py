88862d91102148cd5f7988ecffe0115b62580e68
AlexVonB <AlexVonB@users.noreply.github.com>
2025-05-24 11:43:09 +0200

    added timemachine table prefix

--- timemachine/models.py
+++ timemachine/models.py
@@ -7,6 +7,7 @@ from datetime import datetime
 
 
 class Device(db.Model):
+    __tablename__ = "ws_device"
     id: Mapped[int] = mapped_column(primary_key=True)
     mac: Mapped[str] = mapped_column(server_default="")
     name: Mapped[str] = mapped_column(server_default="")
@@ -19,6 +20,7 @@ class Device(db.Model):
 
 
 class Card(db.Model):
+    __tablename__ = "ws_card"
     id: Mapped[int] = mapped_column(primary_key=True)
     rfid: Mapped[str]
     name: Mapped[str]
@@ -29,10 +31,11 @@ class Card(db.Model):
 
 
 class CardAction(db.Model):
+    __tablename__ = "ws_card_action"
     id: Mapped[int] = mapped_column(primary_key=True)
-    device_id: Mapped[int] = mapped_column(ForeignKey("device.id"))
+    device_id: Mapped[int] = mapped_column(ForeignKey("ws_device.id"))
     device: Mapped["Device"] = relationship(back_populates="card_actions")
-    card_id: Mapped[int] = mapped_column(ForeignKey("card.id"))
+    card_id: Mapped[int] = mapped_column(ForeignKey("ws_card.id"))
     card: Mapped["Card"] = relationship(back_populates="card_actions")
     type: Mapped[str]  # checkin, checkout
     processed: Mapped[bool] = mapped_column(server_default="f")
